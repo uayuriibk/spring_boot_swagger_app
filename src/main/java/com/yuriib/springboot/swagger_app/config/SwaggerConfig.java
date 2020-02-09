ceecb41135470b54d5df4141737ea70d32c87776
yuriybk <email2yurii@gmail.com>
2020-02-09 04:39:45 +0200

    Configuration class with Docket has been added

---
+++ src/main/java/com/yuriib/springboot/swagger_app/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.yuriib.springboot.swagger_app.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("com.yuriib.springboot.swagger_app.controller"))
+                .paths(PathSelectors.regex("/.*"))
+                .build().apiInfo(apiEndPointsInfo());
+    }
+
+    private ApiInfo apiEndPointsInfo() {
+        return new ApiInfoBuilder().title("Spring Boot Rest API + Swagger 2.0")
+                .description("Items Management REST API")
+                .contact(new Contact("Yurii", "www.nowhere.com", "emаil2yurii@gmаil.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.1.2")
+                .build();
+    }
+}
